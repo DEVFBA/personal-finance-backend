524baa7c59dee93ba9bb749a2e6af588ab0e3733
Angel Gutierrez <angenrique.gutierrez@gmail.com>
2021-05-19 09:28:52 -0500

    Finishing Saving Goal CRUD

--- controllers/savingGoals.js
+++ controllers/savingGoals.js
@@ -22,7 +22,33 @@ function getSavingGoals(req, res, next) {
     }
 }
 
+function deleteSavingGoal(req, res) {
+  // únicamente borra a su propio usuario obteniendo el id del token
+  SavingGoal.findOneAndDelete({ _id: req.params.id }).then(r => {         //Buscando y eliminando usuario en MongoDB.
+    res.status(200).send(`Saving Goal ${req.params.id} deleted: ${r}`);
+  })
+}
+
+function updateSavingGoal(req, res, next) {
+  //console.log(req.user)
+  SavingGoal.findById({ _id: req.params.id }).then(savingGoal => {
+    if (!savingGoal) { return res.sendStatus(401); }
+    let newInfo = req.body
+    if (typeof newInfo.amountSaved !== 'undefined')
+      savingGoal.amountSaved = newInfo.amountSaved
+    if (typeof newInfo.targetAmount !== 'undefined')
+      savingGoal.targetAmount = newInfo.targetAmount
+    if (typeof newInfo.targetDate !== 'undefined')
+    savingGoal.targetDate = newInfo.targetDate
+    savingGoal.save().then(updatedSavingGoal => { 
+      res.status(201).json(updatedSavingGoal.publicData())
+    }).catch(next)
+  }).catch(next)
+}
+
 module.exports = {
     createSavingGoal,
-    getSavingGoals
+    getSavingGoals,
+    deleteSavingGoal,
+    updateSavingGoal
 }
--- routes/savingGoals.js
+++ routes/savingGoals.js
@@ -10,7 +10,7 @@ var auth = require('./auth');
 router.get('/', auth.required, getSavingGoals)
 router.get('/:id', auth.required, getSavingGoals)// nuevo endpoint con todos los detalles de mascota
 router.post('/', auth.required, createSavingGoal)
-//router.put('/:id',auth.required, updateSavingGoal)
-//router.delete('/:id',auth.required, deleteSavingGoal)
+router.put('/:id', auth.required, updateSavingGoal)
+router.delete('/:id', auth.required, deleteSavingGoal)
 
 module.exports = router;
