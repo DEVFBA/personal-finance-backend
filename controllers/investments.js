4a505849c980976d4520180e81b4c50f7a7cf92b
Angel Gutierrez <angenrique.gutierrez@gmail.com>
2021-05-22 16:25:31 -0500

    Adding Update/Delete Investment

--- controllers/investments.js
+++ controllers/investments.js
@@ -22,7 +22,31 @@ function getInvestments(req, res, next) {
     }
 }
 
+function deleteInvestment(req, res) {
+  // únicamente borra a su propio usuario obteniendo el id del token
+  Investment.findOneAndDelete({ _id: req.params.id }).then(r => {         //Buscando y eliminando usuario en MongoDB.
+    res.status(200).send(`Investment ${req.params.id} deleted: ${r}`);
+  })
+}
+
+function updateInvestment(req, res, next) {
+  //console.log(req.user)
+  Investment.findById({ _id: req.params.id }).then(investment => {
+    if (!investment) { return res.sendStatus(401); }
+    let newInfo = req.body
+    if (typeof newInfo.investedAmount !== 'undefined')
+      investment.investedAmount = newInfo.investedAmount
+    if (typeof newInfo.total !== 'undefined')
+      investment.total = newInfo.total
+    investment.save().then(updatedInvestment => { 
+      res.status(201).json(updatedInvestment.publicData())
+    }).catch(next)
+  }).catch(next)
+}
+
 module.exports = {
     createInvestment,
-    getInvestments
+    getInvestments,
+    deleteInvestment,
+    updateInvestment
 }
--- routes/investments.js
+++ routes/investments.js
@@ -10,7 +10,7 @@ var auth = require('./auth');
 router.get('/', auth.required, getInvestments)
 router.get('/:id', auth.required, getInvestments)// nuevo endpoint con todos los detalles de mascota
 router.post('/', auth.required, createInvestment)
-//router.put('/:id',auth.required, updateSavingGoal)
-//router.delete('/:id',auth.required, deleteSavingGoal)
+router.put('/:id',auth.required, updateInvestment)
+router.delete('/:id',auth.required, deleteInvestment)
 
 module.exports = router;
